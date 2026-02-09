e625b5879987ddb746798a9fda40880b60a64235
Nishchitha Etige <sandeep.drck@gmail.com>
2026-02-09 08:53:19 -0700

    updated figures

---
+++ config.py
@@ -0,0 +1,17 @@
+# Use config.py to set global variables.
+
+# Liz's dir:
+# input_dir = '/mnt/rd-data/Projects/p2702/Mengnan/output'
+# output_dir = '/mnt/rd-data/Projects/p2702/Mengnan/output'
+# input_dir_grace = '/glade/work/mengnanz/TELLUS_GRAC-GRFO_MASCON_CRI_GRID_RL06.1_V3/'  # Contains this: 'GRCTellus.JPL.200204_202211.GLO.RL06.1M.MSCNv03CRI.nc'
+# input_dir_ecco = '\project_static\ocean\mzhao\ECCOv4r5'  # ECCO-GRID.nc is in this dir
+
+input_dir = '/glade/u/home/mengnanz/p2702/scripts/saved_data/'
+output_dir = './saved_data/'
+
+input_dir_grace = '/glade/work/mengnanz/TELLUS_GRAC-GRFO_MASCON_CRI_GRID_RL06.1_V3_202404/'  # Contains this: 'GRCTellus.JPL.200204_202211.GLO.RL06.1M.MSCNv03CRI.nc'
+
+input_dir_ecco_v4r5_ext = '/glade/work/mengnanz/V4r5_ext_2020_2024_Feb/OBP_mon_mean_202001_202402'  
+input_dir_ecco_v4r5_to2019 = '/glade/u/home/mengnanz/p2375_bp_seasonal_cycle/input_dir/v4r5_to2019/OBP_mon_mean'
+input_dir_ecco_ctrl = '/glade/work/mengnanz/V4r5_ctrl/OBP_mon'
+input_dir_ecco_grid = '/glade/u/home/mengnanz/p2375_bp_seasonal_cycle/input_dir/ECCOllc90/r5_nctiles_grid' # contains ECCO-GRID.nc
